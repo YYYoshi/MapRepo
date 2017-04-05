2e2deb794ac1efd974b9c94af941839e386484cc
YYY <ichigo3cake@gmail.com>
2017-04-05 21:58:23 +0900

    新規プロジェクト

---
+++ Map/Map/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace Map
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var context = this.DataContext as MainWindowViewModel;
+            if (context == null) { return; }
+
+            // JavaScript の関数を呼び出す。
+            this._webBrowser.InvokeScript("moveMap", context.Address);
+        }
+    }
+}
---
+++ Map/Map/MainWindowViewModel.cs
@@ -0,0 +1,67 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Windows.Input;
+
+namespace Map
+{
+    class MainWindowViewModel : BindableBase
+    {
+        public MainWindowViewModel()
+        {
+        }
+
+        private string _uri = String.Format("file://{0}GeoJsonLoad.html", AppDomain.CurrentDomain.BaseDirectory);
+        public string Uri
+        {
+            get { return _uri; }
+
+        }
+
+        private string address;
+        /// <summary>
+        /// Google Map に指定する住所を取得または設定します。
+        /// </summary>
+        public string Address
+        {
+            get
+            {
+                return address;
+            }
+            set
+            {
+                SetProperty(ref address, value);
+            }
+        }
+        public ICommand GoCommand
+        {
+            get
+            {
+                return new DelegateCommand(MoveMap);
+            }
+        }
+
+        private void MoveMap()
+        {
+            // JavaScript の関数を呼び出す。
+            //this._webBrowser.InvokeScript("moveMap", Address);
+        }
+
+
+        private string mapHost;
+        /// <summary>
+        /// Google Map に指定する住所を取得または設定します。
+        /// </summary>
+        public string MapHost
+        {
+            get
+            {
+                return mapHost;
+            }
+            set
+            {
+                SetProperty(ref mapHost, value);
+            }
+        }
+    }
+}
