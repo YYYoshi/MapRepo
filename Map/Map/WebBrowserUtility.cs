900194a90e5e01183cce3425346f7baf8a4f5a7c
YYY <ichigo3cake@gmail.com>
2017-04-05 22:02:30 +0900

    添付プロパティを移動

--- Map/Map/WebBrowserUtility.cs
+++ Map/Map/WebBrowserUtility.cs
@@ -48,5 +48,45 @@ namespace Map
         /// WebBrowser クラスの Source プロパティへの Binding 機能を提供する為の依存プロパティです。
         /// </summary>
         public static readonly DependencyProperty BindableSourceProperty = DependencyProperty.RegisterAttached("BindableSource", typeof(string), typeof(WebBrowserUtility), new UIPropertyMetadata(null, BindableSourcePropertyChanged));
+
+
+        /// <summary>
+        /// ObjectForScriptingProperty を取得します。
+        /// </summary>
+        /// <param name="obj">依存プロパティ。</param>
+        /// <returns>取得した値。</returns>
+        public static string GetObjectForScripting(DependencyObject obj)
+        {
+            return (string)obj.GetValue(ObjectForScriptingProperty);
+        }
+
+        /// <summary>
+        /// ObjectForScriptingProperty を設定します。
+        /// </summary>
+        /// <param name="obj">依存プロパティ。</param>
+        /// <param name="value">設定する値。</param>
+        public static void SetObjectForScripting(DependencyObject obj, string value)
+        {
+            obj.SetValue(ObjectForScriptingProperty, value);
+        }
+
+        /// <summary>
+        /// ObjectForScriptingProperty が変更された時に発生します。
+        /// </summary>
+        /// <param name="o">依存プロパティ。</param>
+        /// <param name="e">イベント データ。</param>
+        public static void OnObjectForScriptingPropertyChanged(DependencyObject o, DependencyPropertyChangedEventArgs e)
+        {
+            WebBrowser browser = o as WebBrowser;
+            if (browser != null)
+            {
+                browser.ObjectForScripting = e.NewValue;
+            }
+        }
+
+        /// <summary>
+        /// ObjectForScripting プロパティへの Binding 機能を提供する為の依存プロパティです。
+        /// </summary>
+        public static readonly DependencyProperty ObjectForScriptingProperty = DependencyProperty.RegisterAttached("ObjectForScripting", typeof(object), typeof(WebBrowserUtility), new UIPropertyMetadata(OnObjectForScriptingPropertyChanged));
     }
 }
